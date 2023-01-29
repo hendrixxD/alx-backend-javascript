ab22c2357e613f4557d189a3ed2c0623b878544c
hendrixxD <maticsjnr@gmail.com>
2023-01-29 11:16:27 +0100

    utils.js

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,21 @@
+export function uploadPhoto() {
+	return Promise.resolve({
+		'status': 200,
+		'body': 'photo-profile-1'
+	});
+}
+
+
+/*export function createUser() {
+	return Promsise.resolve({
+		'firstName': 'Guillaume',
+		'lastName': 'Salva'
+	});
+}
+*/
+export createUser() => {
+        return Promsise.resolve({
+                'firstName': 'Guillaume',
+                'lastName': 'Salva'
+        });
+}
