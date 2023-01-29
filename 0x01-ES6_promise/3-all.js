4116dfe9c12ab00782e747ade387e81e8ad92b0d
hendrixxD <maticsjnr@gmail.com>
2023-01-29 11:29:05 +0100

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,18 @@
+from './utils' import { uploadPhoto, createUser };
+
+export default function handleProfileSignup() {
+	const photo = uploadPhoto();
+	const user = createUser();
+
+	return Promise.all([photo, user])
+	.then((value) => {
+		const profile = value[0].body;
+		const first = value[1].firstName;
+		const last = value[1].lastName;
+
+		console.log(
+			`${profile} ${first} ${last}`,
+		);
+	})
+	.catch(() => console.log('Signup system offline'));
+}
