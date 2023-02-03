8af2ca7bc72ea2f8e7d6b95724e86b64b0f789ed
hendrixxD <maticsjnr@gmail.com>
2023-02-03 23:02:47 +0100

    camelCase

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const new_class = [
+  const newClass = [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
   ];
 
-  return new_class;
+  return newClass;
 }
