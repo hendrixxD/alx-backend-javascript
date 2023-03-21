ef7e03a3344e394e662fc5c47bacb0a3b526b6e9
hendrixxD <maticsjnr@gmail.com>
2023-03-22 00:08:35 +0100

    rad file syncronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,37 @@
 const fs = require('fs');
 
-function countStudents(path) {
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
   try {
-    const data = fs.readFileSync(path, 'utf-8').trim();
-    const records = data.split('\n').map((record) => record.split(',').map((field) => field.trim()));
-
-    // remove empty records (if any)
-    records.filter((record) => record.length > 1);
-
-    // count number of students in each field
-    const fields = records.map((record) => record[3]);
-    const fieldCounts = {};
-    fields.forEach((field) => {
-      fieldCounts[field] = (fieldCounts[field] || 0) + 1;
-    });
-
-    // display results
-    console.log(`Number of students: ${records.length}`);
-    for (const field in fieldCounts) {
-      const count = fieldCounts[field];
-      const students = records.filter((record) => record[3] === field).map((record) => record[0]);
-      console.log(`Number of students in ${field}: ${count}. List: ${students.join(', ')}`);
+    const fileContents = fs.readFileSync(fileName, 'utf-8');
+    const lines = fileContents.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
+    }
+    const l = length - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
     }
   } catch (error) {
-    console.log(`Cannot load the database: ${error.message}`);
+    throw Error('Cannot load the database');
   }
 }
 
